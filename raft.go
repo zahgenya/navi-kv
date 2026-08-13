f98b72146b5b161792588b2e3f2dd2219785838d
Yevhenii Zakharchenko <zahgenya@gmail.com>
2026-08-13 16:12:04 +0300

    fix: typo in atoi

--- raft.go
+++ raft.go
@@ -4,9 +4,11 @@ import (
 	"bufio"
 	"crypto/rand"
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"io"
 	mathrand "math/rand"
+	"net"
 	"net/http"
 	"net/rpc"
 	"os"
@@ -43,7 +45,7 @@ type ServerState string
 
 const (
 	leaderState    ServerState = "leader"
-	followedState              = "follower"
+	followerState              = "follower"
 	candidateState             = "candidate"
 )
 
@@ -334,4 +336,548 @@ func (s *Server) setVotedFor(id uint64) {
 	}
 
 	ServerAssert(s, "Invalid cluster", true, false)
+}
+
+func (s *Server) Start() {
+	s.mu.Lock()
+	s.state = followerState
+	s.done = false
+	s.mu.Unlock()
+
+	s.restore()
+
+	rpcServer := rpc.NewServer()
+	rpcServer.Register(s)
+	l, err := net.Listen("tcp", s.address)
+	if err != nil {
+		panic(err)
+	}
+	mux := http.NewServeMux()
+	mux.Handle(rpc.DefaultRPCPath, rpcServer)
+
+	s.server = &http.Server{Handler: mux}
+	go s.server.Serve(l)
+
+	go func() {
+		s.mu.Lock()
+		s.resetElectionTimeout()
+		s.mu.Unlock()
+
+		for {
+			s.mu.Lock()
+			if s.done {
+				s.mu.Unlock()
+				return
+			}
+			state := s.state
+			s.mu.Unlock()
+
+			switch state {
+			case leaderState:
+				s.heartbeat()
+				s.advanceCommitIndex()
+			case followerState:
+				s.timeout()
+				s.advanceCommitIndex()
+			case candidateState:
+				s.timeout()
+				s.becomeLeader()
+			}
+		}
+	}()
+}
+
+func (s *Server) resetElectionTimeout() {
+	interval := time.Duration(mathrand.Intn(s.heartbeatMs*2) + s.heartbeatMs*2)
+	s.debugf("new interval: %s", interval*time.Millisecond)
+}
+
+func (s *Server) timeout() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	hasTimedOut := time.Now().After(s.electionTimeout)
+	if hasTimedOut {
+		s.debug("timed out, starting new election")
+		s.state = candidateState
+		s.currentTerm++
+		for i := range s.cluster {
+			if i == s.clusterIndex {
+				s.cluster[i].votedFor = s.id
+			} else {
+				s.cluster[i].votedFor = 0
+			}
+		}
+
+		s.resetElectionTimeout()
+		s.persist(false, 0)
+		s.requestVote()
+	}
+}
+
+type RPCMessage struct {
+	Term uint64
+}
+
+type RequestVoteRequest struct {
+	RPCMessage
+	// candidate requesting vote
+	CandidateId uint64
+	// index of candidate's last log entry
+	LastLogIndex uint64
+	// term of candidate's last log entry
+	LastLogTerm uint64
+}
+
+type RequestVoteResponse struct {
+	RPCMessage
+	// true means candidate got vote
+	VoteGranted bool
+}
+
+func (s *Server) requestVote() {
+	for i := range s.cluster {
+		if i == s.clusterIndex {
+			continue
+		}
+
+		go func() {
+			s.mu.Lock()
+
+			s.debugf("requesting vote from %d", s.cluster[i].Id)
+
+			lastLogIndex := uint64(len(s.log) - 1)
+			lastLogTerm := s.log[len(s.log)-1].Term
+
+			req := RequestVoteRequest{
+				RPCMessage: RPCMessage{
+					Term: s.currentTerm,
+				},
+				CandidateId:  s.id,
+				LastLogIndex: lastLogIndex,
+				LastLogTerm:  lastLogTerm,
+			}
+			s.mu.Unlock()
+
+			var rsp RequestVoteResponse
+			ok := s.rpcCall(i, "Server.HandleRequestVoteRequest", req, &rsp)
+			if !ok {
+				return
+			}
+
+			s.mu.Lock()
+			defer s.mu.Unlock()
+
+			if s.updateTerm(rsp.RPCMessage) {
+				return
+			}
+
+			dropStaleResponse := rsp.Term != req.Term
+			if dropStaleResponse {
+				return
+			}
+
+			if rsp.VoteGranted {
+				s.debugf("vote granted by %d", s.cluster[i].Id)
+				s.cluster[i].votedFor = s.id
+			}
+		}()
+	}
+}
+
+func (s *Server) updateTerm(msg RPCMessage) bool {
+	transitioned := false
+	if msg.Term > s.currentTerm {
+		s.currentTerm = msg.Term
+		s.state = followerState
+		s.setVotedFor(0)
+		transitioned = true
+		s.debug("transitioned to follower")
+		s.resetElectionTimeout()
+		s.persist(false, 0)
+	}
+	return transitioned
+}
+
+func (s *Server) rpcCall(i int, name string, req, rsp any) bool {
+	s.mu.Lock()
+	c := s.cluster[i]
+	var err error
+	var rpcClient *rpc.Client = c.rpcClient
+	if c.rpcClient == nil {
+		c.rpcClient, err = rpc.DialHTTP("tcp", c.Address)
+		rpcClient = c.rpcClient
+	}
+	s.mu.Unlock()
+
+	// TODO: implement reconnection
+
+	if err == nil {
+		err = rpcClient.Call(name, req, rsp)
+	}
+
+	if err != nil {
+		s.warnf("error calling %s on %d: %s", name, c.Id, err)
+	}
+
+	return err == nil
+}
+
+func (s *Server) HandleRequestVoteRequest(req RequestVoteRequest, rsp *RequestVoteResponse) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.updateTerm(req.RPCMessage)
+
+	s.debugf("recieved vote request message from %d", req.CandidateId)
+
+	rsp.VoteGranted = false
+	rsp.Term = s.currentTerm
+
+	if req.Term < s.currentTerm {
+		s.debugf("not granting vote request from %d.", req.CandidateId)
+		ServerAssert(s, "VoteGranted = false", rsp.VoteGranted, false)
+		return nil
+	}
+
+	lastLogTerm := s.log[len(s.log)-1].Term
+	logLen := uint64(len(s.log) - 1)
+	logOk := req.LastLogTerm > lastLogTerm ||
+		(req.LastLogTerm == lastLogTerm && req.LastLogIndex >= logLen)
+	grant := req.Term == s.currentTerm &&
+		logOk &&
+		(s.getVotedFor() == 0 || s.getVotedFor() == req.CandidateId)
+	if grant {
+		s.debugf("voted for %d", req.CandidateId)
+		s.setVotedFor(req.CandidateId)
+		rsp.VoteGranted = true
+		s.resetElectionTimeout()
+		s.persist(false, 0)
+	} else {
+		s.debugf("not granting vote request from %d", +req.CandidateId)
+	}
+
+	return nil
+}
+
+func (s *Server) becomeLeader() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	quorum := len(s.cluster)/2 + 1
+	for i := range s.cluster {
+		if s.cluster[i].votedFor == s.id && quorum > 0 {
+			quorum--
+		}
+	}
+
+	if quorum == 0 {
+		// reset all cluster state
+		for i := range s.cluster {
+			s.cluster[i].nextIndex = uint64(len(s.log) + 1)
+			// even match index is reset. Address Figure 2 from Raft
+			// it shows both new index and match index are reset after every election
+			s.cluster[i].matchIndex = 0
+		}
+
+		s.debug("new leader")
+		s.state = leaderState
+
+		// ref: Section 8 Client Interaction:
+		// First a leader must have the latest information
+		// on which entries are committed. The Leader
+		// Completeness Property gurantees that a leader
+		// has all committed entries, but at the
+		// start of its term, it may not know which those are.
+		// To find out, it needs to commit an entry
+		// from its term. Raft handles this by having each
+		// leader commit a blank no-op entry into the log
+		// at the start of its term
+		s.log = append(s.log, Entry{Term: s.currentTerm, Command: nil})
+		s.persist(true, 1)
+
+		s.heartbeatTimeout = time.Now()
+	}
+}
+
+var ErrApplyToLeader = errors.New("cannot apply message to follower, apply to leader")
+
+func (s *Server) Apply(commands [][]byte) ([]ApplyResult, error) {
+	s.mu.Lock()
+	if s.state != leaderState {
+		s.mu.Unlock()
+		return nil, ErrApplyToLeader
+	}
+	s.debugf("processing %d new entry", len(commands))
+
+	resultChans := make([]chan ApplyResult, len(commands))
+	for i, command := range commands {
+		resultChans[i] = make(chan ApplyResult)
+		s.log = append(s.log, Entry{
+			Term:    s.currentTerm,
+			Command: command,
+			result:  resultChans[i],
+		})
+	}
+
+	s.persist(true, len(commands))
+	s.debug("waiting to be applied")
+	s.mu.Unlock()
+
+	s.appendEntries()
+	// TODO: what to do if it takes too long
+	results := make([]ApplyResult, len(commands))
+	var wg sync.WaitGroup
+	wg.Add(len(commands))
+	for i, ch := range resultChans {
+		go func(i int, c chan ApplyResult) {
+			results[i] = <-c
+			wg.Done()
+		}(i, ch)
+	}
+
+	wg.Wait()
+
+	return results, nil
+}
+
+type AppendEntriesRequest struct {
+	RPCMessage
+
+	// so follower can redirect clients
+	LeaderId uint64
+
+	// index of log entry immediately preceding new ones
+	PrevLogIndex uint64
+
+	// term of prevLogIndex entry
+	PrevLogTerm uint64
+
+	// log entries to store. Empty for heartbeat
+	Entries []Entry
+
+	// leader's commitIndex
+	LeaderCommit uint64
+}
+
+type AppendEntriesResponse struct {
+	RPCMessage
+
+	Success bool
+}
+
+const MAX_APPEND_ENTRIES_BATCH = 8_000
+
+func (s *Server) appendEntries() {
+	for i := range s.cluster {
+		// no need to send message to self
+		if i == s.clusterIndex {
+			continue
+		}
+
+		go func(i int) {
+			s.mu.Lock()
+
+			next := s.cluster[i].nextIndex
+			prevLogIndex := next - 1
+			prevLogTerm := s.log[prevLogIndex].Term
+
+			var entries []Entry
+			if uint64(len(s.log)-1) >= s.cluster[i].nextIndex {
+				s.debugf("len: %d, next: %d, server: %d", len(s.log), next, s.cluster[i].Id)
+				entries = s.log[next:]
+			}
+
+			// keep latency down by only applying N at a time
+			if len(entries) > MAX_APPEND_ENTRIES_BATCH {
+				entries = entries[:MAX_APPEND_ENTRIES_BATCH]
+			}
+
+			lenEntries := uint64(len(entries))
+			req := AppendEntriesRequest{
+				RPCMessage: RPCMessage{
+					Term: s.currentTerm,
+				},
+				LeaderId:     s.cluster[s.clusterIndex].Id,
+				PrevLogIndex: prevLogIndex,
+				PrevLogTerm:  prevLogTerm,
+				Entries:      entries,
+				LeaderCommit: s.commitIndex,
+			}
+
+			s.mu.Unlock()
+
+			var rsp AppendEntriesResponse
+			s.debugf("sending %d entries to %d for term %d", len(entries), s.cluster[i].Id, req.Term)
+			ok := s.rpcCall(i, "Server.HandleAppendEntriesRequest", req, &rsp)
+			if !ok {
+				return
+			}
+
+			s.mu.Lock()
+			defer s.mu.Unlock()
+			if s.updateTerm(rsp.RPCMessage) {
+				return
+			}
+
+			dropStaleResponse := rsp.Term != req.Term && s.state == leaderState
+			if dropStaleResponse {
+				return
+			}
+
+			if rsp.Success {
+				prev := s.cluster[i].nextIndex
+				s.cluster[i].nextIndex = max(req.PrevLogIndex+lenEntries+1, 1)
+				s.cluster[i].matchIndex = s.cluster[i].nextIndex - 1
+				s.debugf("message accepted for %d. Prev Index: %d, Next Index: %d, Match Index: %d", s.cluster[i].Id, prev, s.cluster[i].nextIndex, s.cluster[i].matchIndex)
+			} else {
+				s.cluster[i].nextIndex = max(s.cluster[i].nextIndex-1, 1)
+				s.debugf("forced to go back to %d for: %d", s.cluster[i].nextIndex, s.cluster[i].Id)
+			}
+		}(i)
+	}
+}
+
+func (s *Server) HandleAppendEntriesRequest(req AppendEntriesRequest, rsp *AppendEntriesResponse) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.updateTerm(req.RPCMessage)
+
+	// if AppendEntries RPC received from new leader: convert to follower
+	if req.Term == s.currentTerm && s.state == candidateState {
+		s.state = followerState
+	}
+
+	rsp.Term = s.currentTerm
+	rsp.Success = false
+
+	if s.state != followerState {
+		s.debugf("non-follower cannot append entries")
+		return nil
+	}
+
+	if req.Term < s.currentTerm {
+		s.debugf("dropping request from old leader %d: term %d", req.LeaderId, req.Term)
+		return nil
+	}
+
+	s.resetElectionTimeout()
+
+	logLen := uint64(len(s.log))
+	validPreviousLog := req.PrevLogIndex == 0 || // induction step
+		(req.PrevLogIndex < logLen &&
+			s.log[req.PrevLogIndex].Term == req.PrevLogTerm)
+	if !validPreviousLog {
+		s.debugf("not a valid log")
+		return nil
+	}
+
+	next := req.PrevLogIndex + 1
+	nNewEntries := 0
+
+	for i := next; i < next+uint64(len(req.Entries)); i++ {
+		e := req.Entries[i-next]
+		if i >= uint64(cap(s.log)) {
+			newTotal := next + uint64(len(req.Entries))
+			// second argument must be `i`
+			// not `0` otherwise the copy after this
+			// doesn't work.
+			// Only copy until `i`, not `newTotal` since
+			// we'll continue appending after this.
+			newLog := make([]Entry, i, newTotal*2)
+			copy(newLog, s.log)
+			s.log = newLog
+		}
+
+		if i < uint64(len(s.log)) && s.log[i].Term != e.Term {
+			prevCap := cap(s.log)
+			// if an existing entry conflicts with a new
+			// one(same index but different terms),
+			// delete the existing entry and all that follow it
+			s.log = s.log[:i]
+			ServerAssert(s, "capacity remains the same while we truncate", cap(s.log), prevCap)
+		}
+
+		s.debugf("appending entry: %s. At index: %d", string(e.Command), len(s.log))
+
+		if i < uint64(len(s.log)) {
+			ServerAssert(s, "existing log is the same as new log", s.log[i].Term, e.Term)
+		} else {
+			s.log = append(s.log, e)
+			ServerAssert(s, "length is directly related to the index", uint64(len(s.log)), i+1)
+			nNewEntries++
+		}
+	}
+
+	if req.LeaderCommit > s.commitIndex {
+		s.commitIndex = min(req.LeaderCommit, uint64(len(s.log)-1))
+	}
+
+	s.persist(nNewEntries != 0, nNewEntries)
+
+	rsp.Success = true
+	return nil
+}
+
+func (s *Server) advanceCommitIndex() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if s.state == leaderState {
+		lastLogIndex := uint64(len(s.log) - 1)
+
+		for i := lastLogIndex; i > s.commitIndex; i-- {
+			quorum := len(s.cluster)/2 + 1
+
+			for j := range s.cluster {
+				if quorum == 0 {
+					break
+				}
+
+				isLeader := j == s.clusterIndex
+				if s.cluster[j].matchIndex >= i || isLeader {
+					quorum--
+				}
+			}
+
+			if quorum == 0 {
+				s.commitIndex = i
+				s.debugf("new commit index: %d", i)
+				break
+			}
+		}
+	}
+
+	if s.lastApplied <= s.commitIndex {
+		log := s.log[s.lastApplied]
+
+		if len(log.Command) != 0 {
+			s.debugf("entry applied: %d", s.lastApplied)
+			res, err := s.statemachine.Apply(log.Command)
+
+			// will be nil for follower entries and for no op entries.
+			// Not nil for all user submitted messages
+			if log.result != nil {
+				log.result <- ApplyResult{
+					Result: res,
+					Error:  err,
+				}
+			}
+		}
+
+		s.lastApplied++
+	}
+}
+
+func (s *Server) heartbeat() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	timeForHeartbeat := time.Now().After(s.heartbeatTimeout)
+	if timeForHeartbeat {
+		s.heartbeatTimeout = time.Now().Add(time.Duration(s.heartbeatMs) * time.Millisecond)
+		s.debugf("sending heartbeat")
+		s.appendEntries()
+	}
 }
